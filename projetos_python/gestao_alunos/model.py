8be601f42fc115b2464f094a835d7936c8da3ba0
álef monteiro <72264836+alef-monteiro@users.noreply.github.com>
2024-09-27 02:34:22 -0400

    adding crud at model

--- projetos_python/gestao_alunos/model.py
+++ projetos_python/gestao_alunos/model.py
@@ -1,2 +1,22 @@
 class School:
     def __init__(self):
+        self.students = {}
+
+    # CREATE
+    def add_student(self, name, grade):
+        self.students[name] = grade
+
+    # DELETE
+    def delete_students(self, key):
+        del self.students[key]
+
+    # UPDATE
+    def updating_student(self, name, new_grade):
+        if name in self.students:
+            self.students[name] = new_grade
+            return True
+        else:
+            return False
+
+    # READ
+    def show_students(self):
